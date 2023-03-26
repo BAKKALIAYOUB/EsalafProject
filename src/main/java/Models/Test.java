305dc10bf1e7d11b72d89945a58a9d1079dcd5b5
BAKKALIAYOUB <ayoubbakkali484@gmail.com>
2023-03-26 17:37:56 +0000

    First Commit

--- src/main/java/Models/Test.java
+++ src/main/java/Models/Test.java
@@ -5,12 +5,9 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws SQLException {
-        Client c0 = new Client(1 , "Salma" , "0634490082");
-        Client c1 = new Client(9 , "Rime" , "0539330510");
         ClientDAO ClientModels = new ClientDAO();
 
-        ClientModels.delete(8);
-
+        ClientModels.save(new Client("hamid" , "test" , "test" , "test"));
 
     }
 }
