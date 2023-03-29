8613df744cfe27d54a504d5c0f2e49b29204847f
BAKKALIAYOUB <ayoubbakkali484@gmail.com>
2023-03-29 18:00:46 +0000

    Nouveau Attribut TotalProduits , TotalCrédit pour les afficher au tableau de la view Client

--- src/main/java/Models/Client.java
+++ src/main/java/Models/Client.java
@@ -5,22 +5,65 @@ public class Client {
     private String nom;
     private String telephone;
 
+    private Float TotalProduits;
+
+    private Float TotalCrédit;
+    private int id_admin;
+
+    public Client( int id_client , String nom, String telephone, Float totalProduits, Float totalCrédit) {
+        this.nom = nom;
+        this.id_client = id_client;
+        this.telephone = telephone;
+        TotalProduits = totalProduits;
+        TotalCrédit = totalCrédit;
+    }
+
     public Client(int id_client, String nom, String telephone) {
         this.id_client = id_client;
         this.nom = nom;
         this.telephone = telephone;
     }
 
-    public Client(String nom, String telephone, String email, String password) {
+    public Client(int id_client, String nom, String telephone, int id_admin) {
+        this.id_client = id_client;
+        this.nom = nom;
+        this.telephone = telephone;
+        this.id_admin = id_admin;
+    }
+
+    public Client(String nom, String telephone) {
         this.nom = nom;
         this.telephone = telephone;
     }
+
 
     public Client() {
 
     }
 
+    public Float getTotalProduits() {
+        return TotalProduits;
+    }
 
+    public void setTotalProduits(Float totalProduits) {
+        TotalProduits = totalProduits;
+    }
+
+    public Float getTotalCrédit() {
+        return TotalCrédit;
+    }
+
+    public void setTotalCrédit(Float totalCrédit) {
+        TotalCrédit = totalCrédit;
+    }
+
+    public int getId_admin() {
+        return id_admin;
+    }
+
+    public void setId_admin(int id_admin) {
+        this.id_admin = id_admin;
+    }
 
     public int getId_client() {
         return id_client;
