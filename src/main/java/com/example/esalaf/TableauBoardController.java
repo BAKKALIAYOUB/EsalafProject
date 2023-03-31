5572d2301e3c4ce791bdb2f5a641bdfb5addb0f9
BAKKALIAYOUB <ayoubbakkali484@gmail.com>
2023-03-31 12:30:09 +0000

    Tableau du bord controller : Envoi des informations sur l'admin connecter au views produit et client

--- src/main/java/com/example/esalaf/TableauBoardController.java
+++ src/main/java/com/example/esalaf/TableauBoardController.java
@@ -21,7 +21,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class TableauBoardController implements Initializable{
+public class TableauBoardController {
 
     @FXML
     private Label TotalProduits;
@@ -32,7 +32,7 @@ public class TableauBoardController implements Initializable{
     @FXML
     private Label WelcomeLabel;
 
-    private MarketAdmin adminLogin = new MarketAdmin();
+    public MarketAdmin adminLogin = new MarketAdmin();
 
     public void setAdminLogin(MarketAdmin object){
         this.adminLogin = object;
@@ -40,16 +40,20 @@ public class TableauBoardController implements Initializable{
     public MarketAdmin getAdminLogin(){
         return this.adminLogin;
     }
+    public int getId(){
+        return this.adminLogin.getId_admin();
+    }
 
-    public void initialize(URL url, ResourceBundle resourceBundle){
+    public void initialize(){
         ProduitDAO produitModel = new ProduitDAO();
         ClientDAO clientModel = new ClientDAO();
         CreditDAO creditModel = new CreditDAO();
 
+        System.out.println(this.getAdminLogin().toString());
 
         int Nombreproduit = produitModel.getNombreProduits();
-        int NombreCleint = clientModel.getNombreCliet();
-        float TotalCredit1 = creditModel.getSommeCredit();
+        int NombreCleint = clientModel.getNombreClient(this.getAdminLogin().getId_admin());
+        float TotalCredit1 = creditModel.getSommeCredit(this.getAdminLogin().getId_admin());
 
         TotalProduits.setText(Integer.toString(Nombreproduit));
         TotalClient.setText(Integer.toString(NombreCleint));
@@ -72,6 +76,21 @@ public class TableauBoardController implements Initializable{
     }
 
     @FXML
+    protected void onProduitsClick(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Produits-view.fxml"));
+        Parent root = loader.load();
+
+        //send Admin object that log in to TableauBordController
+        ProduitsController produitsController = loader.getController();
+        produitsController.setAdmin(this.getAdminLogin());
+
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root );
+        stage.setScene(scene);
+    }
+
+    @FXML
     protected void onClientsClick(ActionEvent event) throws IOException , SQLException{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Client-view.fxml"));
         Parent root = loader.load();
@@ -90,8 +109,9 @@ public class TableauBoardController implements Initializable{
     }
 
     public void setWelcomeMessage(){
-        System.out.println(this.getAdminLogin().toString());
+        System.out.println(this.adminLogin.getId_admin());
         WelcomeLabel.setText("Welcome " + this.adminLogin.getNom());
+
     }
 
 
