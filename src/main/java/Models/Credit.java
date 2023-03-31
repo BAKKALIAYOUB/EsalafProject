0d899d72a1648f69b576306aebba729f15aa0a58
BAKKALIAYOUB <ayoubbakkali484@gmail.com>
2023-03-31 12:27:47 +0000

    Crédit Model

--- src/main/java/Models/Credit.java
+++ src/main/java/Models/Credit.java
@@ -4,8 +4,33 @@ public class Credit {
     private int id_client;
     private int id_produit;
     private String date;
-
     private int quantite;
+
+    private String nomProduit;
+
+    private float prix;
+    private float total;
+
+    public int getId_crédit() {
+        return id_crédit;
+    }
+
+    public void setId_crédit(int id_crédit) {
+        this.id_crédit = id_crédit;
+    }
+
+    private int id_crédit;
+
+
+
+    public Credit(int id_crédit, String date, int quantite, String nomProduit, float prix , float f) {
+        this.date = date;
+        this.quantite = quantite;
+        this.nomProduit = nomProduit;
+        this.prix = prix;
+        this.id_crédit = id_crédit;
+        this.total = f;
+    }
 
     public Credit(int id_client, int id_produit, String date, int quantite) {
         this.id_client = id_client;
@@ -47,5 +72,42 @@ public class Credit {
 
     public void setQuantite(int quantite) {
         this.quantite = quantite;
+    }
+
+    public String getNomProduit() {
+        return nomProduit;
+    }
+
+    public void setNomProduit(String nomProduit) {
+        this.nomProduit = nomProduit;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Credit{" +
+                "id_client=" + id_client +
+                ", id_produit=" + id_produit +
+                ", date='" + date + '\'' +
+                ", quantite=" + quantite +
+                ", nomProduit='" + nomProduit + '\'' +
+                ", prix=" + prix +
+                ", total=" + total +
+                '}';
     }
 }
--- src/main/java/Models/CreditDAO.java
+++ src/main/java/Models/CreditDAO.java
@@ -1,8 +1,11 @@
 package Models;
 
+import javafx.scene.control.Button;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreditDAO extends BaseDAO<Credit>{
@@ -23,11 +26,21 @@ public class CreditDAO extends BaseDAO<Credit>{
 
     @Override
     public void update(Credit object) throws SQLException {
+        String req = "UPDATE crédit SET quantité = ? WHERE id_crédit = ?";
+
+        this.preparedStatement = this.connection.prepareStatement(req);
+        this.preparedStatement.setInt(1 , object.getQuantite());
+        this.preparedStatement.setInt(2 , object.getId_crédit());
+
+        this.preparedStatement.execute();
+
+
 
     }
 
     @Override
     public void delete(int id) throws SQLException {
+
 
     }
 
@@ -41,21 +54,77 @@ public class CreditDAO extends BaseDAO<Credit>{
         return null;
     }
 
-    public float getSommeCredit() {
-        String req = "SELECT SUM(p.prix * c.quantité) AS TotalCredit FROM produit p JOIN crédit c ON p.id_produit = c.id_produit";
-        Statement statement = null;
+    public float getSommeCredit(int id) {
+        String req = "SELECT SUM(p.prix * c.quantité) AS TotalCredit FROM produit p " +
+                        "JOIN crédit c ON p.id_produit = c.id_produit " +
+                        "JOIN client cl ON cl.id_client = c.id_client " +
+                        "JOIN marketadmin m ON m.id_admin = cl.id_admin " +
+                        "WHERE cl.id_admin = ?";
         try {
-            statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(req);
+            this.preparedStatement = this.connection.prepareStatement(req);
+            this.preparedStatement.setInt(1,id);
+            this.resultSet = this.preparedStatement.executeQuery();
             float total = 0;
-            if(result.next()){
-                total = result.getFloat("TotalCredit");
+            if(this.resultSet.next()){
+                total = this.resultSet.getFloat("TotalCredit");
             }
             return total;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public List<Credit> getCreditAsClient(int id) throws SQLException{
+        List<Credit> sqlCredit = new ArrayList<Credit>();
+        String req = "SELECT crédit.id_crédit, quantité, date , produit.nom , produit.prix " +
+                "FROM crédit , produit " +
+                "WHERE id_client = ? AND crédit.id_produit= produit.id_produit; ";
+
+        this.preparedStatement = this.connection.prepareStatement(req);
+        this.preparedStatement.setInt(1 , id);
+
+        this.resultSet = this.preparedStatement.executeQuery();
+
+        while (this.resultSet.next()){
+            float total = this.resultSet.getFloat("prix") * this.resultSet.getInt("quantité");
+            sqlCredit.add(
+                    new Credit(
+                            this.resultSet.getInt("id_crédit"),
+                            this.resultSet.getString("date"),
+                            this.resultSet.getInt("quantité"),
+                            this.resultSet.getString("nom"),
+                            this.resultSet.getFloat("prix"),
+
+                            total
+
+                    )
+            );
+        }
+        return sqlCredit;
+    }
+
+    public void saveAsClient(Credit c , int id_produit , int id_client) throws SQLException{
+        String req = "INSERT INTO crédit (`id_produit`, `id_client`, `quantité`, `date`) VALUES" +
+                " (? , ? , ? , ?)";
+        this.preparedStatement = this.connection.prepareStatement(req);
+        this.preparedStatement.setInt(1 , id_produit);
+        this.preparedStatement.setInt(2 , id_client);
+        this.preparedStatement.setInt(3 , c.getQuantite());
+        this.preparedStatement.setString(4 , c.getDate());
+
+        this.preparedStatement.execute();
+    }
+
+    public void deleteRow(Credit object) throws SQLException{
+        String req = "DELETE FROM crédit WHERE id_crédit = ?";
+
+        this.preparedStatement = this.connection.prepareStatement(req);
+        this.preparedStatement.setInt(1 , object.getId_crédit());
+
+
+        this.preparedStatement.execute();
 
     }
+
 }
